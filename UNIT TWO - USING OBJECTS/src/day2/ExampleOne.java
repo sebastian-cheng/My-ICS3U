ced4444547d7433e9d32b15ed264aec2212f5f2c
Sebastian Cheng <scheng1@bayviewglen.ca>
2022-10-12 20:24:08 -0400

    h

---
+++ UNIT TWO - USING OBJECTS/src/day2/ExampleOne.java
@@ -0,0 +1,36 @@
+package day2;
+public class ExampleOne {
+    public static void main(String[] args) {
+        String superStudent = new String("Callum");
+        String baseballPitcher = new String("Nolan Ryan");
+        System.out.println(superStudent);
+        System.out.println("Length of superStudent is " + superStudent.length());
+        System.out.println("Substring of superStudent 2 - 4: " + superStudent.substring(2,4));
+
+        System.out.println(baseballPitcher);
+        System.out.println("Length of baseballPitcher is " + baseballPitcher.length());
+        System.out.println("Substring of baseballPitcher 2 - 4: " + baseballPitcher.substring(2,4));
+        /* int length
+       System.out.println("Length of superStudent is " + superStudent.length());
+        System.out.println("Length of baseballPitcher is " + baseballPitcher.length());
+        */
+        /*substring
+        System.out.println("Substring of superStudent 2 - 4: " + superStudent.substring(2,4));
+        System.out.println("Substring of baseballPitcher 2 - 4: " + baseballPitcher.substring(2,4));
+        Middle of a string is length() / 2
+        First half of a string is substring(0,length()/2)         
+         */
+        System.out.println(baseballPitcher.substring(0,baseballPitcher.length()/2));
+        //second half is length()/2 and go to the end 
+        System.out.println(baseballPitcher.substring(baseballPitcher.length()/2));
+        //auto goes to the end
+
+        //indexOf
+        System.out.println("Where the first 'n' 'Nolan Ryan'?");
+        System.out.println(baseballPitcher.indexOf("n"));
+        
+        System.out.println(baseballPitcher.substring(baseballPitcher.length() -1));
+        }
+        
+    }
+
---
+++ UNIT TWO - USING OBJECTS/src/day2/ExamplecompareTo.java
@@ -0,0 +1,21 @@
+package day2;
+
+/**
+ * ExamplecompareTo
+ */
+public class ExamplecompareTo {
+
+    public static void main(String[] args) {
+        String s1 = new String("Happy");
+        String s2 = new String("Happy");
+        String s3 = "Happy";
+        String s4 = "Happy";
+        //two equal signs for equality 
+        //one for assignment
+        System.out.println(s1==s2); //false bc different addresses 
+        
+        System.out.println(s1.equals(s2)); //true bc same sequence of characters 
+       // s3 == s4 because string literals are not reused
+        System.out.println();
+    }
+}
